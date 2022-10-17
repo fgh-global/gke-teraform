932f79d7fc073a2c0bef252e7ad8af2f64a90218
joenjie <113528586+joenjie@users.noreply.github.com>
2022-10-17 11:03:06 -0400

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -13,14 +13,12 @@ variable "gke_num_nodes" {
   description = "number of gke nodes"
 }
 
-# GKE cluster
+# GKE cluster.
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
   
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
+  
   remove_default_node_pool = true
   initial_node_count       = 1
 
